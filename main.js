29b0f1b2b69ee9c5ffb1e5506fde3e7fd9e59fd2
Vanessa F <vfarmer1@live.com>
2021-03-16 16:42:03 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,221 @@
+var genres = [
+    'mystery', 'murder-mystery', 'dystopian future', 'science-fiction', 'fantasy', 'historical fiction', 'Western',
+    'romance', 'horror', 'suspense', 'adventure', 'space opera', 'alternate history', 'steampunk', 'romantic science-fiction',
+    'military science-fiction', 'legal thriller', 'psychological thriller', 'espionage', 'medical thriller', 'comedy',
+    'paranormal thriller', 'cozy mystery', 'romantic suspense', 'regency romance', 'paranormal romance', 'fable',
+    'tragedy', 'young adult', 'crime', 'folklore', 'realistic fiction', 'fairy tale', 'fanfiction'
+]
+
+var protagonists = [
+    'detective', 'doctor', 'driving instructor', 'zookeeper', 'concierge', 'tour guide',
+    'farmer', 'florist', 'actor/actress', 'cake decorator', 'bike courier', 'tattoo artist',
+    'veterinarian', 'midwife', 'bounty hunter', 'elevator mechanic', 'journalist', 'investigative journalist',
+    'forensic scientist', 'funeral service manager', 'meterologist', 'nurse', 'CIA analyst',
+    'robotics engineer', 'hotel manager', 'web developer', 'event planner', 'counselor', 
+    'full-time student'
+]
+
+var proNames = [
+    'Holly Terry','Bonnie Thomas','Frankie Frost','Maj Fox','Genevieve Mcguire',
+    'Veronica Lawrence','Elena Parrish','Stacey Patel','Gertrude Palmer',
+    'Catherine McCullough','Madeleine Boyd','Jasmine Craig','Aaliyah Parsons',
+    'Anika Fields','Katy Dawson','India Hamilton','Constance Mathis','Robin Dean',
+    'Anne Shannon','Diana Bates','Talia Roberts','Jinni Snow','Ava Hatfield',
+    'Haleema McGowan','Robbie Guerrero','Laila Howe','Lissa Cox','Ellie Lester',
+    'Abigail Harrington','Georgia Farmer','Kimberly Nash','Crystal Atkins',
+    'Felix Johns','Karly Eaton','Claudia Harrell','Viktoria Sparks','Chicka Gay',
+    'Jamie Baker','Bertha Glenn','Danielle Wagner','Eliza Welsh','Gloria Crane',
+    'Annabel Hull','Yasmine Calderon','Maddie Espinoza','Ilsa Tran','Rosie Hernandez',
+    'Vincent Ayala','Lenny Cruz','Gethin Patrick','Omar Johns','Pim Koch',
+    'Leroy Mays','Matteo Howe','Roy Waters','Keaton Quinn','Barnaby Olsen',
+    'Harvey Jennings','Owen Bonner','Arjun Cole','Jay Figueroa','Sashenka Vaughn',
+    'Curtis Foley','Leonardo Huff','Travis Peters','Maxim Woodward','Victor Fleming',
+    'Bilal Cummings','Kyron Marks','Erik Page','Rusty Ochoa','Jack Owens',
+    'Imani Walters','Casey Cox','Ioan Gay','Aron Hodges','Kira Blaese','Nora Moon',
+    'Chaz Holt','Kevin Gallegos','Allen Hunter','Caleb Valenzuela','Violet Cordova',
+    'Rick Hurst','Ray Montoya','Muhammad Rasmussen','Haroon Sherman','Ivan Cuevas',
+    'Marcus Wright','Aoife Booker','Verity Rowland','Jeffrey Robinson'
+]
+
+var proFlaws = [
+    'trusting too easily', 'rebelling against authority', 'vanity', 'assuming the worst about others',
+    'being lazy', 'setting extremely high standards for him/herself', 'holding grudges', 'substance abuse',
+    'being placed under a curse', 'a neurotic illness that interferes with their daily life',
+    'being fixated on an event/events that took place in the past', 
+    'being purposefully antisocial, driving themselves to isolation', 'having a phobia' 
+]
+
+var proQuests = [
+    'discover his/her origin story',
+    'defeat an evil force in his/her life',
+    'get the guy/girl that he/she is secretly in love with',
+    'go on a long journey to retrieve or return a dangerous artifact',
+    'learn how to wield his/her new skills or powers',
+    'apprehend a villain whose identity is unknown',
+    'survive a dangerous environment/situation and return home safely',
+    'rescue someone they love who is in danger',
+    'uncover the truth',
+    'free the people from an oppressive tyrant'
+]
+
+var antagonists = [
+    'falcon trainer', 'aerial photographer', 'scientist', 'politician',
+    'cybersecurity expert', 'stock market trader', 'mobster', 'professor',
+    'doctor', 'pharmacist', 'architect', 'ballet teacher', 'clockmaker',
+    'CIA analyst', 'forensic scientist', 'diplomat', 'spy', 'full-time-student',
+    'priest', 'circus performer', 'marketing manager', 'registered nurse',
+    'software developer', 'customer service representative', 'accountant',
+    'security guard', 'occupational therapist', 'police officer', 
+    'gentleman/lady of leisure', 'musician'
+]
+
+var antNames = [
+    'Luis Hansen','George Nelson','Norman Corona','Dean Knox','Dion Dominique',
+    'Adenoid Ridgeway','Dan Maher','Hansen Bates','Vito Eyler','Yang Xinhai',
+    'Teo Kraft','Frits Peter','Mallory Cady','Kat Jessup','Maxine Earle',
+    'Rose Cole','Dee Jeffrey','Esme Chester','Ava Desmond','Nella West',
+    'Charlotte Rome'
+]
+
+var antFlaws = [
+    'arrogance', 'not doing background checks on his/her henchmen', 'being cowardly',
+    'having poorly-executed plans', 'being extremely superstitious',
+    'being terrified of the ghosts of the person/people he/she killed',
+    'having a deviated septum'
+]
+
+var antQuests = [
+    'take over the world',
+    'achieve power in some form',
+    'steal a precious artifact',
+    'ruin the protagonist\'s reputation over a grudge',
+    'convince a certain population of a lie that will benefit the antagonist',
+    'steal the protagonist\'s loved one away out of spite or jealousy',
+    'defeat the protagonist in some sort of high-stakes contest',
+    'keep the protagonist from realizing their true potential',
+    'kidnap someone the protagonist loves so that they can meet the protagonist in person and fight',
+    'make a great discovery that leads to terrible consequences',
+    'get away with a crime they committed long ago'
+]
+
+var climaxes = [
+    'A well-loved character suddenly dies in the company of the protagonist, raising suspicion',
+    'A minor character is revealed to be a villain, and the protagonist and antagonist put aside their differences to fight him/her',
+    'The protagonist reveals the actions of the antagonist to the other characters, who were blind to them this whole time',
+    'A villain more cruel than the antagonist kidnaps both the protagonist and antagonist. They are trapped together and must confront each other\'s points of views',
+    'There is a duel between the protagonist and the antagonist',
+    'The protagonist learns the antagonist is not the villain of the story--everything he/she knows is a lie',
+    'The protagonist finds a letter that explains something important to the plot of the story'
+]
+
+var herrings = [
+    'An innocent character is arrested, but not for the crime being investigated. They committed a minor crime (such as shoplifting), but this leaves the readers in suspense.',
+    'The protagonist is being stalked by an innocent character. This is actually because the character suspects the protagonist.',
+    'There is an ornate locked box in the house that a certain character guards very carefully. The readers expect one thing to be inside, but they are surprised by what ends up being in there instead.',
+    'An innocent character visits the same place every day and then does not on the day of an incident, leading readers to believe him/her guilty.',
+    'After an innocent character introduces themselves, the protagonist notices that he/she drops their wallet, and they did not give the name on their ID. Suspicious!',
+    'An innocent character who is a reformed convict was the last person to see another character alive.',
+]
+
+var disasters = [
+    'The protagonist is severely wounded and has to hole up in his/her place for several days, delaying their quest.',
+    'The protagonist is fired from his/her job',
+    'The protagonist loses any special skills/powers they may have',
+    'The protagonist finds out someone they care about has died',
+    'The protagonist sacrifices their freedom to ensure their one true love can live.',
+    'The protagonist\'s love interest misunderstands a situation and angrily breaks up with him/her.',
+    'The protagonist\'s mentor turns out to be a traitor'
+]
+
+var romances = [
+    'The protagonist realizes that he/she was in front of him/her...the whole time.',
+    'The protagonist rescues their friend from a villain, and in that moment realizes how much they care for him/her.',
+    'The protagonist gets in a fight with their sidekick who then leaves. Only then do they realize how much they miss each other.',
+    'The protagonist receives an anonymous love note and does not know who it\'s from. To the audience, it is obvious. But the protagonist eventually falls in love with its author.',
+    'The protagonist proposes to their love interest in the beginning of the book, and they are rejected. After their valiant efforts in their quest throughout the book, the two naturally fall in love.',
+    'The protagonist and their love interest have been friends since birth, and neither will say how they feel towards each other until the end of the book.'
+]
+
+var resolutions = [
+    'A mystery is solved',
+    'A villain is apprehended',
+    'Love conquers all',
+    'Family is reunited',
+    'Evil is defeated',
+    'The government is overthrown',
+    'The truth is uncovered',
+    'A war ends',
+    'A precious item is returned to its rightful owner(s)',
+    'The protagonist overcomes their fears',
+    'The protagonist finds true love'
+]
+
+function newGenre(){
+    var randomNumber = Math.floor(Math.random() * (genres.length));
+    document.getElementById('displayGenre').innerHTML = genres[randomNumber];
+}
+
+function newProtagonist(){
+    var randomNumber = Math.floor(Math.random() * (protagonists.length));
+    document.getElementById('protagonist').innerHTML = protagonists[randomNumber];
+}
+
+function newProtagonistName(){
+    var randomNumber = Math.floor(Math.random() * (proNames.length));
+    document.getElementById('protagonistName').innerHTML = proNames[randomNumber];
+}
+
+function newFlawProtagonist(){
+    var randomNumber = Math.floor(Math.random() * (proFlaws.length));
+    document.getElementById('fatalFlawProtagonist').innerHTML = proFlaws[randomNumber];
+}
+
+function newQuestProtagonist(){
+    var randomNumber = Math.floor(Math.random() * (proQuests.length));
+    document.getElementById('questProtagonist').innerHTML = proQuests[randomNumber];
+}
+
+function newAntagonist(){
+    var randomNumber = Math.floor(Math.random() * (antagonists.length));
+    document.getElementById('antagonist').innerHTML = antagonists[randomNumber];
+}
+
+function newAntagonistName(){
+    var randomNumber = Math.floor(Math.random() * (antNames.length));
+    document.getElementById('antagonistName').innerHTML = antNames[randomNumber];
+}
+
+function newFlawAntagonist(){
+    var randomNumber = Math.floor(Math.random() * (antFlaws.length));
+    document.getElementById('fatalFlawAntagonist').innerHTML = antFlaws[randomNumber];
+}
+
+function newQuestAntagonist(){
+    var randomNumber = Math.floor(Math.random() * (antQuests.length));
+    document.getElementById('questAntagonist').innerHTML = antQuests[randomNumber];
+}
+
+function newClimax(){
+    var randomNumber = Math.floor(Math.random()* (climaxes.length));
+    document.getElementById('climax').innerHTML = climaxes[randomNumber];
+}
+
+function newHerring(){
+    var randomNumber = Math.floor(Math.random()* (herrings.length));
+    document.getElementById('redHerring').innerHTML = herrings[randomNumber];
+}
+
+function newDisaster(){
+    var randomNumber = Math.floor(Math.random()* (disasters.length));
+    document.getElementById('unexpectedDisaster').innerHTML = disasters[randomNumber];
+}
+
+function newRomance(){
+    var randomNumber = Math.floor(Math.random()* (romances.length));
+    document.getElementById('romance').innerHTML = romances[randomNumber];
+}
+
+function newResolution(){
+    var randomNumber = Math.floor(Math.random()* (resolutions.length));
+    document.getElementById('resolution').innerHTML = resolutions[randomNumber];
+}
